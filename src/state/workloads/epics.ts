f153529c55f0cd6425090e9baa56af3e8cc3da81
Lakshay <10690117+lakshayrastogi@users.noreply.github.com>
2021-04-24 17:01:58 +0530

    Phase 1 done - enabled users to create new workloads

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,10 +1,12 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, map, tap, ignoreElements } from 'rxjs/operators';
+import { filter, map, tap, ignoreElements, mergeMap } from 'rxjs/operators';
 import { isActionOf } from 'typesafe-actions';
 
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
+import { WorkloadService } from './services'
 
+const workloadService = new WorkloadService
 
 type AppEpic = Epic<RootAction, RootAction, RootState>;
 
@@ -17,9 +19,20 @@ const logWorkloadSubmissions: AppEpic = (action$, state$) => (
   )
 );
 
+const submitWorkload: AppEpic = (action$, state$) => (
+  action$.pipe(
+  	filter(isActionOf(workloadsActions.submit)),
+  	mergeMap(async (action) => {
+  		const createdCall = await workloadService.create(action.payload)
+  		return workloadsActions.created(createdCall)
+  	})
+  )
+);
+
 
 export const epics = combineEpics(
   logWorkloadSubmissions,
+  submitWorkload
 );
 
 export default epics;
